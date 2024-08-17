ab82ec10e6340d34bb4eb8df979b1f261ca282b0
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-17 09:40:11 +0530

    Log Info msg & Warning msg

--- src/main/java/lk/ijse/Logging.java
+++ src/main/java/lk/ijse/Logging.java
@@ -1,2 +1,14 @@
-package lk.ijse;public class Logging {
+package lk.ijse;
+
+import java.util.logging.Logger;
+
+public class Logging {
+
+    private static final Logger logger = Logger.getLogger(Logging.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("imfo msg");
+        logger.warning("Warning msg");
+    }
+
 }
